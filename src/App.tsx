afa84d962409da2c559188e70169720bec58cd39
Alexey Sh <nor80486@gmail.com>
2022-12-24 12:22:46 +0300

    heheh

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import TextContent from './TextContent';
+import { CounterComponent } from './CounterComponent';
+import { Input } from './Input';
+import assetsImage from './Assets/img.png'
 
 function App() {
+  const handler = () => {
+    console.log('КНОПКА была нажата');
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TextContent/>
-      </header>
+      <CounterComponent title={'Orange'} count={9} />
+      <CounterComponent title={'Apple'} count={4} />
+      <CounterComponent title={'Lemon'}/>
+        <button onClick= {handler}>PUSH ME</button>
+        <Input />
+
+        <img src={assetsImage} alt="img" />
+
+        <img src={'/img.png'} alt="img" /> {/* этот способ лучше */}
+
+        <div className='example'>HELLO WORLD</div>
     </div>
   );
 }
